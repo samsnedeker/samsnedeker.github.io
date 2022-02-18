514c4eee110b055c5488417142b6788102848e54
Sam Snedeker <snedekersa@gmail.com>
2022-02-18 13:05:02 -0500

    pic, medium link

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ function createLink(url, text) {
         </a>
     )
 }
+
 function MainContent() {
     return (
         <div className="container">
+            <a href="https://www.companioninabox.art/">
+                <img src={ ("images/companioninabox.png") } id="profile-image"/>
+            </a>
             <h1>Samantha Snedeker</h1>
             <p className="bio">I like reading, writing, and coding.</p> 
         </div>
@@ -20,6 +24,7 @@ function Socials() {
             <li>{createLink("https://www.linkedin.com/in/samanthasnedeker/","LinkedIn")}</li>
             <li>{createLink("https://twitter.com/snedsas","Twitter")}</li>
             <li>{createLink("https://github.com/samsnedeker","GitHub")}</li>
+            <li>{createLink("https://medium.com/thoughts-from-an-undergrad/running-hills-90faf0379afa","Medium")}</li>
         </div>
     )
 }
