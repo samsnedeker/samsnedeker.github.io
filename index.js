9a9076e8284ce8d845bd8d4cef961ae899be5728
Sam Snedeker <snedekersa@gmail.com>
2022-02-18 12:26:17 -0500

    social media link function

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+function socialMediaLink(url, text) {
+    return (
+        <a href={url} className={text}>
+            {text}
+        </a>
+    )
+}
 function MainContent() {
     return (
         <div className="main content">
             <h1>Samantha Snedeker</h1>
             <body className="bio">I like to write. I like to code. I love reading and I love learning.</body> 
-            <body>
-                <a href="https://www.linkedin.com/in/samanthasnedeker/"
-                    className="linkedin">      
-                    LinkedIn   
-                </a>
-            </body>
+            <ul>
+                <li>{socialMediaLink("https://www.linkedin.com/in/samanthasnedeker/","LinkedIn")}</li>
+                <li>{socialMediaLink("https://twitter.com/snedsas","Twitter")}</li>
+            </ul>
         </div>
-
     )
 }
  
