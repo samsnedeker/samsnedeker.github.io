1312f8404c30fa752f6dc51b366a6cd9d1803d55
Sam Snedeker <snedekersa@gmail.com>
2022-02-18 13:25:54 -0500

    add dune link

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function MainContent() {
             </a>
             <h1>Samantha Snedeker</h1>
             <p id="bio">I like reading, writing, and coding.</p> 
-            <p id="socials"> {createLink("https://www.linkedin.com/in/samanthasnedeker/","LinkedIn")} | {createLink("https://twitter.com/snedsas","Twitter")} | {createLink("https://github.com/samsnedeker","GitHub")} | {createLink("https://medium.com/thoughts-from-an-undergrad/running-hills-90faf0379afa","Medium")} | {createLink("https://www.goodreads.com/user/show/131479645-samantha","GoodReads")} </p>
+            <p id="socials"> {createLink("https://www.linkedin.com/in/samanthasnedeker/","LinkedIn")} | {createLink("https://twitter.com/snedsas","Twitter")} | {createLink("https://github.com/samsnedeker","GitHub")} | {createLink("https://medium.com/thoughts-from-an-undergrad/running-hills-90faf0379afa","Medium")} | {createLink("https://www.goodreads.com/user/show/131479645-samantha","GoodReads")} | {createLink("https://dune.xyz/snedsa", "Dune")} </p>
             <form className="readingrecs">
                 <label for="fname">What are you reading right now?</label><br></br>
                 <input type="text" id="fname" name="fname"></input><br></br>
