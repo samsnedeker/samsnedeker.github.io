82829eaf6d341f58c2f4110f568e1ad0c1aa03d4
Sam Snedeker <snedekersa@gmail.com>
2022-02-18 12:09:09 -0500

    add linkedin

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 function MainContent() {
     return (
-        <div>
-            <h1 className="main content">Samantha Snedeker</h1>
+        <div className="main content">
+            <h1>Samantha Snedeker</h1>
             <body className="bio">I like to write. I like to code. I love reading and I love learning.</body> 
+            <body>
+                <a href="https://www.linkedin.com/in/samanthasnedeker/"
+                    className="linkedin">      
+                    LinkedIn   
+                </a>
+            </body>
         </div>
 
+    )
+}
+
+function createLink() {
+    var a = document.createElement('a');
+    a.setAttribute('href', 'https://www.linkedin.com/in/samanthasnedeker/');
+    a.innerHTML = 'LinkedIn';
+    return (
+        <div>
+            <body>hi</body>
+        </div>
     )
 }
  
 ReactDOM.render(
     <div>
         <MainContent></MainContent>
+        <createLink></createLink>
     </div>,
     document.getElementById("root")
 )
